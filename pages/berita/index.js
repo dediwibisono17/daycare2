976df1a30f77885ab39f7c9f918fc963db392647
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2023-06-21 14:41:52 +0700

    c

---
+++ pages/berita/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../components/Shared/Header/Header";
+import Berita from "../../views/Berita/Berita";
+import Footer from "../../components/Shared/Footer/Footer";
+
+const BeritaPage = (props) => {
+    return (
+      <>
+
+        <Berita></Berita>
+      </>
+    );
+  };
+  
+  export default BeritaPage;
